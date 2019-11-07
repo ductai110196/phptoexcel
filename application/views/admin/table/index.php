51c023487aa18649f31ae0500a1d5a966f914d24
NguyenDucTai <tai9331@gmail.com>
2019-11-07 13:46:10 +0700

    1:46

--- application/views/admin/table/index.php
+++ application/views/admin/table/index.php
@@ -3,6 +3,11 @@ $name = "Tables_in_" . $this->session->userdata("db");
 ?>
 <h1 class="h3 mb-4 text-gray-800 text-center font-weight-bold">Danh Sách Bảng</h1>
 <hr>
+<?php
+if ($before_head ==  "") {
+    echo ('<span class="mx-5 text-danger text-center">Vui lòng chọn cơ sỡ dữ liệu!</span>');
+} else {
+    ?>
 <table class="table table-hover table-bordered table-responsive-sm">
     <thead>
         <tr class="bg-success text-white">
@@ -13,7 +18,7 @@ $name = "Tables_in_" . $this->session->userdata("db");
     </thead>
     <tbody>
         <?php
-        foreach ($before_head as $key => $item) { ?>
+                foreach ($before_head as $key => $item) { ?>
         <tr>
             <td><?php echo $key + 1 ?></td>
             <td><?php echo $item->$name ?></td>
@@ -30,3 +35,4 @@ $name = "Tables_in_" . $this->session->userdata("db");
         <?php } ?>
     </tbody>
 </table>
+<?php } ?>
